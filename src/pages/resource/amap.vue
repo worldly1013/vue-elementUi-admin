56f8415555c9af02b253d8ac4f1c8d6d976f507c
ly <yang.li@fenzhitech.com>
2018-03-21 10:18:15 +0800

    新增高德地图模块

--- src/pages/resource/amap.vue
+++ src/pages/resource/amap.vue
@@ -11,6 +11,7 @@
         </div>
         <div id="amap-container" tabindex="0"></div>
       </div>
+
     </div>
   </div>
 </template>
@@ -23,30 +24,37 @@ import {
 export default {
   data() {
     return {
-      scenicData:[
-        {
-          'url':'http://dev.fenzhitech.com/res/218c5e4e3809e3f77d9c49e3cf581756.jpg',
-          'name':'东方明珠',
-          'coordinate':[121.499809, 31.239666],
-          'flag':false
+      scenicData: [{
+          'url': 'http://dev.fenzhitech.com/res/218c5e4e3809e3f77d9c49e3cf581756.jpg',
+          'name': '东方明珠',
+          'intro':'东方明珠广播电视塔（The Oriental Pearl Radio & TV Tower）是上海的标志性文化景观之一，位于浦东新区陆家嘴，塔高约468米。',
+          'price':999.00,
+          'coordinate': [121.499809, 31.239666],
+          'flag': false
         },
         {
-          'url':'http://dev.fenzhitech.com/res/9f51093effeb8be58a5327e6bcb65357.png',
-          'name':'豫园',
-          'coordinate':[121.491983, 31.227029],
-          'flag':false
+          'url': 'http://dev.fenzhitech.com/res/9f51093effeb8be58a5327e6bcb65357.png',
+          'name': '豫园',
+          'intro':'豫园（Yu Garden）位于上海市老城厢的东北部，北靠福佑路，东临安仁街，西南与上海老城隍庙毗邻[1]  ，是江南古典园林，始建于明代嘉靖、万历年间，占地三十余亩。',
+          'price':199.00,
+          'coordinate': [121.491983, 31.227029],
+          'flag': false
         },
         {
-          'url':'http://dev.fenzhitech.com/res/67a28bb99586b227cb177750fef4b58e.jpg',
-          'name':'上海科技馆',
-          'coordinate':[121.542492, 31.219134],
-          'flag':false
+          'url': 'http://dev.fenzhitech.com/res/67a28bb99586b227cb177750fef4b58e.jpg',
+          'name': '上海科技馆',
+          'intro':'上海科技馆是上海市人民政府为贯彻落实科教兴国战略，提高城市综合竞争力和市民科学文化素养而投资兴建的具有中国特色、时代特征、上海特点的综合性的自然科学技术博物馆。',
+          'price':222.00,
+          'coordinate': [121.542492, 31.219134],
+          'flag': false
         },
         {
-          'url':'http://dev.fenzhitech.com/res/41d00e38d1c8df3be22a83989a340ea7.jpg',
-          'name':'金茂大厦',
-          'coordinate':[121.505823, 31.235315],
-          'flag':false
+          'url': 'http://dev.fenzhitech.com/res/41d00e38d1c8df3be22a83989a340ea7.jpg',
+          'name': '金茂大厦',
+          'intro':'金茂大厦（Jinmao Tower），又称金茂大楼，竣工于1999年，曾经是中国大陆最高的大楼，位于上海市浦东新区黄浦江畔的陆家嘴金融贸易区，楼高420.5米。',
+          'price':188.00,
+          'coordinate': [121.505823, 31.235315],
+          'flag': false
         },
       ]
     }
@@ -55,138 +63,151 @@ export default {
     panelTitle
   },
   mounted() {
-    this.initAMap()
+    this.showAddress(this.scenicData[0],0)
   },
   methods: {
-    initAMap() {
+    showInfoWindow(title,intro,price,coordinate) {
       var map = new AMap.Map('amap-container', {
-        // 是否能调整地图缩放比例
         resizeEnable: true,
-        // 地图比例
-        zoom: 13,
-        // 定位中心点
-        center: [121.480983, 31.2258],
-        // 视图模式
-        // viewMode: '3D',
-        // 3D视图角度
+        zoom: 15,
+        center: coordinate,
         pitch: 20,
-        // 地图样式
         mapStyle: 'amap://styles/blue'
       });
 
-      var lnglats = [
-        [121.491983, 31.227029],    // 豫园
-        [121.499809, 31.239666],    // 东方明珠
-        [121.542492, 31.219134],    // 上海科技馆
-        [121.505823, 31.235315]     // 金茂大厦
-      ];
-
-      //实例化信息窗体
-var title='方恒假日酒店<span style="font-size:11px;color:#F00;">价格:318</span>',
-    content=[];
-content.push("电话：010 64733333");
-content.push("<a target='_blank'>详细信息</a>");
-var infoWindow = new AMap.InfoWindow({
-    isCustom: true,  //使用自定义窗体
-    content: createInfoWindow(title,content.join("<br>")),
-    offset: new AMap.Pixel(16, -50)//-113, -140
-});
-infoWindow.open(map, [121.491983, 31.227029]);
-
-//构建自定义信息窗体
-function createInfoWindow(title, content) {
-    var info = document.createElement("div");
-    info.className = "info";
-
-    //可以通过下面的方式修改自定义窗体的宽高
-    //info.style.width = "400px";
-    // 定义顶部标题
-    var top = document.createElement("div");
-    var titleD = document.createElement("div");
-    var closeX = document.createElement("img");
-    top.className = "info-top";
-    titleD.innerHTML = title;
-    closeX.src = "https://webapi.amap.com/images/close2.gif";
-    closeX.onclick = closeInfoWindow;
-
-    top.appendChild(titleD);
-    top.appendChild(closeX);
-    info.appendChild(top);
-
-    // 定义中部内容
-    var middle = document.createElement("div");
-    middle.className = "info-middle";
-    middle.style.backgroundColor = 'white';
-    middle.innerHTML = content;
-    info.appendChild(middle);
-
-    // 定义底部内容
-    var bottom = document.createElement("div");
-    bottom.className = "info-bottom";
-    bottom.style.position = 'relative';
-    bottom.style.top = '0px';
-    bottom.style.margin = '0 auto';
-    var sharp = document.createElement("img");
-    sharp.src = "https://webapi.amap.com/images/sharp.png";
-    bottom.appendChild(sharp);
-    info.appendChild(bottom);
-    return info;
-}
-
-//关闭信息窗体
-function closeInfoWindow() {
-    map.clearInfoWindow();
-}
-
-      //
-      // var infoWindow = new AMap.InfoWindow({
-      //   offset: new AMap.Pixel(0, -30)
-      // });
-
-    },
-    showAddress(item,index){
-      this.scenicData.forEach((el,i) => {
-        if(i === index){ this.scenicData[i]['flag'] = true}
-        if(i !== index){ this.scenicData[i]['flag'] = false}
+      var marker = new AMap.Marker({
+        map:map,
+        position:coordinate
       })
+
+      //构建自定义信息窗体
+      function createInfoWindow(title,intro,price){
+        var info =   '<div class="info-container">' +
+            '<div class="top">' +
+              '<p>' + title + '</p>' +
+              '<img src="http://dev.fenzhitech.com/res/218c5e4e3809e3f77d9c49e3cf581756.jpg" alt="关闭图片">' +
+            '</div>' +
+            '<div class="middle">' +
+              '<p class="intro">' + intro + '</p>' +
+              '<p class="price">价格：<span>' + price + '</span>元</p>' +
+            '</div>' +
+            '<div class="arrow">' +
+              '<img src="https://webapi.amap.com/images/sharp.png">' +
+            '</div>' +
+           '</div>'
+
+        return info
+      }
+      //关闭信息窗体
+      function closeInfoWindow() {
+        map.clearInfoWindow();
+      }
+      //实例化信息窗体
+      function showInfo(title,intro,price,coordinate){
+        var infoWindow = new AMap.InfoWindow({
+          isCustom: true, //使用自定义窗体
+          content: createInfoWindow(title,intro,price),
+          offset: new AMap.Pixel(10, -55)
+        });
+        infoWindow.open(map,coordinate);
+      }
+
+      return showInfo(title,intro,price,coordinate)
+    },
+    showAddress(item, index) {
+      this.scenicData.forEach((el, i) => {
+        if (i === index) {
+          this.scenicData[i]['flag'] = true
+        }
+        if (i !== index) {
+          this.scenicData[i]['flag'] = false
+        }
+      })
+      // 展示信息窗口
+      this.showInfoWindow(item.name,item.intro,item.price,item.coordinate)
     }
   }
 }
 </script>
 
 <style lang="scss">
-  .scenic-container{
+.scenic-container {
     float: left;
-    width:234px;
+    width: 234px;
     height: 518px;
     overflow-y: scroll;
     padding: 0 10px;
-    .scenic-item{
-      width: 234px;
-      margin-bottom: 10px;
-      border: 1px solid #fff;
-      box-sizing: border-box;
-      img{
-        display: block;
-        width:100%;
-        height:130px;
-      }
-      p{
-        line-height: 20px;
-        padding: 8px;
-        text-align: center;
-      }
+    .scenic-item {
+        width: 234px;
+        margin-bottom: 10px;
+        border: 1px solid #fff;
+        box-sizing: border-box;
+        img {
+            display: block;
+            width: 100%;
+            height: 130px;
+        }
+        p {
+            line-height: 20px;
+            padding: 8px;
+            text-align: center;
+        }
     }
-    .scenic-item.active{
-      border: 1px solid #accaea;
-      box-shadow: 0 0 6px rgba(172, 202, 234, 1);
+    .scenic-item.active {
+        border: 1px solid #accaea;
+        box-shadow: 0 0 6px rgba(172, 202, 234, 1);
     }
 
+}
+.info-container{
+  width: 300px;
+  background-color: #fff;
+  box-sizing:border-box;
+  border: 1px solid #accaea;
+  box-shadow: 0 0 6px rgba(172, 202, 234, 1);
+  .top{
+    position: relative;
+    height:30px;
+    line-height: 30px;
+    background-color:#72AAE3 ;
+    padding: 0 10px;
+    color:white;
+    img{
+      position: absolute;
+      right:10px;
+      top:50%;
+      transform: translate(0,-50%);
+      width: 20px;
+      height: 20px;
+    }
   }
-  #amap-container{
+  .middle{
+    padding: 5px 10px;
+    font-size: 12px;
+    line-height: 22px;
+    .intro{
+      color:#999;
+    }
+    .price{
+      color:#EE1C14;
+    }
+  }
+  .arrow{
+    width: 100%;
+    height:0px;
+    clear:both;
+    text-align: center;
+    img{
+      position: relative;
+      z-index: 100;
+    }
+  }
+}
+
+#amap-container {
     float: right;
-    width:700px;
+    width: 700px;
     height: 518px;
     margin: 0 auto;
-  }
+}
 </style>
